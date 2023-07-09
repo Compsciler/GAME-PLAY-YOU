22ec9f8147e2613d6df6ce306fbf8f3932f97bfa
Roger Wang <rogerwang2520@gmail.com>
2023-07-08 18:48:29 -0700

    Add "Mark Brown" AWS Polly text to speech

---
+++ Assets/Scripts/TextToSpeech.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using Amazon.Polly;
+using Amazon.Polly.Model;
+using Amazon.Runtime;
+
+public class TextToSpeech : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    string textToSpeak = "Hi, my name is mark and I am making a video game about magnets.";
+
+    string configPath;
+    string audioOutputPath;
+
+    void Awake()
+    {
+        string rootPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(Application.dataPath, ".."));
+        configPath = $"{rootPath}/config.json";
+        audioOutputPath = $"{Application.persistentDataPath}/ttsaudio.mp3";
+    }
+
+    async Task Start()
+    {
+        if (!File.Exists(configPath))
+        {
+            Debug.LogError("Config file not found");
+        }
+
+        string json = File.ReadAllText(configPath);
+        ConfigData config = JsonUtility.FromJson<ConfigData>(json);
+        string awsAccessKey = config.awsAccessKey;
+        string awsSecretKey = config.awsSecretKey;
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        AmazonPollyClient client = new AmazonPollyClient(credentials, Amazon.RegionEndpoint.USWest2);
+
+        SynthesizeSpeechRequest sreq = new SynthesizeSpeechRequest
+        {
+            Text = textToSpeak,
+            Engine = Engine.Neural,
+            VoiceId = VoiceId.Arthur,
+            OutputFormat = OutputFormat.Mp3
+        };
+
+        SynthesizeSpeechResponse sres = null;
+
+        sres = await client.SynthesizeSpeechAsync(sreq);
+
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(audioOutputPath, AudioType.MPEG))
+        {
+            var op = www.SendWebRequest();
+            while (!op.isDone) { await Task.Yield(); }
+
+            AudioClip clip = DownloadHandlerAudioClip.GetContent(www);
+
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+
+    private void WriteIntoFile(Stream stream)
+    {
+        using (FileStream fileStream = new FileStream(audioOutputPath, FileMode.Create))
+        {
+            byte[] buffer = new byte[8 * 1024];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                fileStream.Write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class ConfigData
+{
+    public string awsAccessKey;
+    public string awsSecretKey;
+}
